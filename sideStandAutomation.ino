3f4ef7a2b48380558bb9502d5b07139faeebd288
Naman Bansal <naman0105@gmail.com>
2018-05-03 11:24:15 +0530

    Update sideStandAutomation.ino

--- sideStandAutomation.ino
+++ sideStandAutomation.ino
@@ -1,45 +1,38 @@
 #include <Servo.h>
-Servo myservo;  
+Servo myservo;                  // initialising servo motor
 int sensor = 4;                 // tilt sensor as input pin
-int LED = 13;                    // led as output pin
+int LED = 13;                   // led as output pin
 
 boolean switchState = false;   
-int sensorState=0;// current state
+int sensorState=0;              // Setting current state as 0
 
 void openStand()
 {
-  
-   myservo.write(110); 
-  
+   myservo.write(110);          // Function to open bike stand 
 }
 
 void closeStand()
 {
-  
-   myservo.write(20); 
-  
-  
+   myservo.write(20);           // Function to close the bike stand
 }
 
 void setup()
 {
-pinMode(sensor, INPUT);
-digitalWrite(sensor, HIGH);      //set the sensor as HIGH
-pinMode(LED, OUTPUT);
-myservo.attach(LED); 
-myservo.write(15); 
+  pinMode(sensor, INPUT);
+  digitalWrite(sensor, HIGH);      //set the sensor as HIGH
+  pinMode(LED, OUTPUT);
+  myservo.attach(LED); 
+  myservo.write(15); 
 }
 
 void loop()
 {
   sensorState=digitalRead(sensor); //read value of the sensor either high or low
 
-  if(sensorState==LOW)            //when tilt switch lays flat 
+  if(sensorState==LOW)             //when tilt switch lays flat 
   {
     //digitalWrite(LED,HIGH);
     closeStand();
-              
-  
   }
   else
   {
